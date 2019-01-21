7c2ffd94e9106f61f89e3a4345c2985d537fdcf7
Hacer Tilbeç <hacertilbec@std.sehir.edu.tr>
2019-01-21 12:27:49 +0300

    scaled features + KNN

--- Protein Distance Matrix/knnclassifier-scaled.py
+++ Protein Distance Matrix/knnclassifier-scaled.py
@@ -27,81 +27,14 @@ batch_size = 100
 num_iters = 100
 input_size=2016
 
-def createDistanceMatrix2(structure,mean,std_dev,resize_strategy,resize_to,sample_size):
-    coords_list = []
-    model=structure[0]
-    for chain in model.get_list():
-        for residue in chain.get_list():
-            try:
-                coords = residue['CA'].coord
-                coords_list.append(coords)
-            except:
-                continue
-    distance_matrix = []
-    for c in coords_list:
-        coord_dist = []
-        for c_ in coords_list:
-            dist = np.linalg.norm(c-c_) # calculate distance between coordinates of CAs of residues
-            coord_dist.append(dist)
-        distance_matrix.append(coord_dist)
-    distance_matrix = (np.array(distance_matrix)-mean)/std_dev
-    # Resize protein_matrix
-    if resize_strategy == False or len(distance_matrix) == resize_to[0]:
-        return distance_matrix
-    else:
-        if resize_strategy == "strategy1":
-            try:
-                resized = cv2.resize(distance_matrix, (resize_to[0], resize_to[1]), interpolation=cv2.INTER_AREA)
-            except:
-                return []
-        elif resize_strategy == "strategy2":
-            if len(distance_matrix) > resize_to[0]:
-                resized = sampling(distance_matrix, new_shape=resize_to,sample_size=sample_size)
-            else:
-                resized = padding(distance_matrix, new_shape=resize_to,sample_size=sample_size)
-        elif resize_strategy == "strategy3":
-            if len(distance_matrix) > resize_to[0]:
-                resized = sampling_s3(distance_matrix, new_shape=resize_to)
-            else:
-                resized = padding_s3(distance_matrix, new_shape=resize_to)
-        else:
-            print("Not a valid strategy method. Use False, strategy1, strategy2 or strategy3.")
-            return
-    return resized
-
-
-# get structure list and returns protein, distance matrix dictionary
-def DistanceMatrixDict2(structures,mean,std_dev,resize_strategy="strategy1", resize_to=(32,32), removeSymmetry=False,sample_size=None):
-    if resize_strategy == "strategy2" and removeSymmetry == True:
-        print("RemoveSymmetry parameter can not be used with strategy2")
-        return
-    protein_matrix_dict = {}
-    for protein in structures:
-        protein_matrix = createDistanceMatrix2(protein,mean,std_dev,resize_strategy, resize_to,sample_size)
-        if len(protein_matrix) == 0:
-            continue
-        if resize_strategy == "strategy2" or resize_strategy == "strategy3":
-            try:
-                protein_matrix[0].shape[1]
-                for sample in enumerate(protein_matrix):
-                    key = protein.id + "sample" + str(sample[0])
-                    protein_matrix_dict[key] = sample[1].flatten()
-            except:
-                protein_matrix_dict[protein.id] = protein_matrix.flatten()
-        else:
-            if type(protein_matrix) == np.ndarray:
-                if removeSymmetry == True:
-                    protein_matrix = RemoveSymmetry(protein_matrix)
-                else:
-                    protein_matrix = protein_matrix.flatten()
-                protein_matrix_dict[protein.id] = protein_matrix
-    return protein_matrix_dict
-
-
+# LOAD LABEL DICTIONARIES
 with open('pickle files/label_dict.pkl', 'rb') as f:
     label_dict = pickle.load(f)
 with open('pickle files/test_labels.pkl', 'rb') as f:
     scop_label_dict = pickle.load(f)
+
+def scale_features(features,mean,std_dev):
+    return (np.array(features)-mean)/std_dev
 
 # SCOP TEST PDBs
 structures = []
@@ -112,9 +45,9 @@ for pdb in os.listdir("SCOP_Test/"):
     parser = PDB.PDBParser()
     structure = parser.get_structure(pdb, pdb_path)
     structures.append(structure)
+
 matrixdict = DistanceMatrixDict(structures, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
 scop_pdb_samples, scop_features = list(matrixdict.keys()), list(matrixdict.values())
-
 
 train_acc_1,test_acc_1, scop_acc_1 = [],[],[]
 train_acc_5,test_acc_5, scop_acc_5 = [],[],[]
@@ -128,55 +61,46 @@ for fold in range(1,11):
     with open("Folds/{n}_test.txt".format(n=fold),"r") as f:
         for i in f.readlines():
             test_pdbs.append(i.strip("\n"))
-    train_pdbs = train_pdbs
-    test_pdbs = test_pdbs
+    train_pdbs = train_pdbs[:20]
+    test_pdbs = test_pdbs[:20]
 
     print(len(train_pdbs))
     print(len(test_pdbs))
 
-    all_values = []
-    for pdb in train_pdbs:
-        try:
+    train_features, test_features = [],[]
+    train_pdb_samples, test_pdb_samples = [],[]
+
+
+    for i in range(int(len(train_pdbs)/batch_size)+1): # BATCH
+        structures = []
+        for pdb in train_pdbs[i*batch_size: (i+1)*batch_size]: # EACH PDB IN THE BATCH
+            if pdb == ".ipynb_checkpoints":
+                continue
             pdb_path = os.path.join("PDBs", pdb)
             parser = PDB.PDBParser()
             structure = parser.get_structure(pdb, pdb_path)
-            coords_list = []
-            model=structure[0]
-            for chain in model.get_list():
-                for residue in chain.get_list():
-                    try:
-                        coords = residue['CA'].coord
-                        coords_list.append(coords)
-                    except:
-                        continue
-            distance_matrix = []
-            for c in coords_list:
-                coord_dist = []
-                for c_ in coords_list:
-                    dist = np.linalg.norm(c-c_) # calculate distance between coordinates of CAs of residues
-                    coord_dist.append(dist)
-                distance_matrix.append(coord_dist)
-            flatten = []
-            row = 0
-            for i in range(1, len(distance_matrix)):
-                flatten += distance_matrix[row][i:]
-                row+=1
-            all_values+=flatten
-        except:
-            continue
-    mean = np.mean(all_values)
-    std_dev = np.std(all_values)
+            structures.append(structure)
+        matrixdict = DistanceMatrixDict(structures, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
+        pdbs, features = list(matrixdict.keys()), list(matrixdict.values())
 
-    print(mean)
-    print(std_dev)
+        train_pdb_samples+=pdbs
+        train_features+=features
 
+    input_size = len(train_features[0])
 
+    # SCALING
+    train_features = np.array(train_features)
+    mean = np.mean(train_features.flatten())
+    std_dev = np.std(train_features.flatten())
+    train_features = scale_features(train_features,mean,std_dev)
+
+    # AUTOENCODER
     tf.reset_default_graph()
     tf.set_random_seed(42)
 
     X = tf.placeholder(tf.float32, shape=[None, input_size])
-    hidden = fully_connected(X, encoding_size, activation_fn=tf.nn.relu)
-    outputs = fully_connected(hidden, input_size, activation_fn=tf.nn.softmax)
+    hidden = fully_connected(X, encoding_size, activation_fn=tf.nn.elu)
+    outputs = fully_connected(hidden, input_size, activation_fn=None)
 
     reconstruction_loss = tf.reduce_sum(tf.square(outputs - X)) # MSE
 
@@ -188,38 +112,15 @@ for fold in range(1,11):
     init = tf.global_variables_initializer()
 
     sess = tf.InteractiveSession()
-    saver = tf.train.Saver()
     sess.run(init)
 
-
-    model_path = "models/"+strategy+str(filter_size)+str(encoding_size)+".ckpt"
-
-    train_features, test_features = [],[]
-    train_pdb_samples, test_pdb_samples = [],[]
-
-    autoencoder = None
-    for i in range(int(len(train_pdbs)/batch_size)+1): # BATCH
-        structures = []
-        for pdb in train_pdbs[i*batch_size: (i+1)*batch_size]: # EACH PDB IN THE BATCH
-            if pdb == ".ipynb_checkpoints":
-                continue
-            pdb_path = os.path.join("PDBs", pdb)
-            parser = PDB.PDBParser()
-            structure = parser.get_structure(pdb, pdb_path)
-            structures.append(structure)
-        matrixdict = DistanceMatrixDict2(structures, mean, std_dev, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
-        pdbs, features = list(matrixdict.keys()), list(matrixdict.values())
-
-        train_pdb_samples+=pdbs
-        train_features+=features
-        input_size = len(features[0])
-
-    # ITERATION
+    # AUTOENCODER TRAINING
     for iteration in range(num_iters):
         for i in range(int(len(train_features)/batch_size)+1): # BATCH
             batch = train_features[i*batch_size: (i+1)*batch_size]
             sess.run([training_op], feed_dict={X: batch})
 
+    # TEST FEATURES
     for i in range(int(len(test_pdbs)/batch_size)+1): # BATCH
         structures = []
         for pdb in test_pdbs[i*batch_size: (i+1)*batch_size]: # EACH PDB IN THE BATCH
@@ -229,11 +130,16 @@ for fold in range(1,11):
             parser = PDB.PDBParser()
             structure = parser.get_structure(pdb, pdb_path)
             structures.append(structure)
-        matrixdict = DistanceMatrixDict2(structures, mean, std_dev, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
+        matrixdict = DistanceMatrixDict(structures, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
         pdbs, features = list(matrixdict.keys()), list(matrixdict.values())
         test_pdb_samples+=pdbs
         test_features+=features
 
+    # SCALING
+    test_features = scale_features(test_features,mean,std_dev)
+    scop_features = scale_features(scop_features,mean,std_dev)
+
+    # REPORT
     print("number of train pdbs: {n}".format(n=len(train_pdb_samples)))
     print("number of test pdbs: {n}".format(n=len(test_pdb_samples)))
     # Embedding vectors of train and test set
@@ -304,7 +210,7 @@ for fold in range(1,11):
     X_scop = np.array(X_scop)
     y_scop = np.array(list(map(lambda x: group2id[x], y_scop)))
 
-
+    # CLASSIFICATION
     knn = KNeighborsClassifier(n_neighbors=1)
     knn.fit(X_train,y_train)
     train_acc_1.append(knn.score(X_train,y_train))
