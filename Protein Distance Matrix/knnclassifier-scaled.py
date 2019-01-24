e2f3074561757b6f4813f32d0dfd5ebb96a5da4f
Hacer Tilbeç <hacertilbec@std.sehir.edu.tr>
2019-01-24 12:33:57 +0300

    refactoring

--- Protein Distance Matrix/knnclassifier-scaled.py
+++ Protein Distance Matrix/knnclassifier-scaled.py
@@ -36,22 +36,84 @@ with open('pickle files/test_labels.pkl', 'rb') as f:
 def scale_features(features,mean,std_dev):
     return (np.array(features)-mean)/std_dev
 
-# SCOP TEST PDBs
-structures = []
-for pdb in os.listdir("SCOP_Test/"):
-    if not pdb.endswith(".pdb"):
-        continue
-    pdb_path = os.path.join("SCOP_Test", pdb)
-    parser = PDB.PDBParser()
-    structure = parser.get_structure(pdb, pdb_path)
-    structures.append(structure)
+def calculate_mean_std(data):
+    features = np.array(data)
+    mean = np.mean(features.flatten())
+    std_dev = np.std(features.flatten())
+    return mean, std_dev
 
-matrixdict = DistanceMatrixDict(structures, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
-scop_pdb_samples, scop_features = list(matrixdict.keys()), list(matrixdict.values())
+def create_fdict(pdbs,features):
+    feature_dict = {}
+    for i in enumerate(pdbs):
+        if "sample" in i[1]:
+            pdb = i[1].split("sample")[0]
+        else:
+            pdb = i[1]
+        feature_dict.setdefault(pdb,[])
+        feature_dict[pdb].append(features[i[0]])
+    return feature_dict
+
+def X_y(feature_dict,label_dict):
+    X_train,y_train = [], []
+    for pdb,vector in feature_dict.items():
+        pdb = pdb.split(".")[0]
+        X_train.append(np.average(vector,axis=0))
+        y_train.append(".".join(label_dict[pdb].split(".")[:2]))
+    return np.array(X_train),y_train
+
+
+class Autoencoder:
+    def __init__(self):
+        self.sess = None
+
+    def build_model(self):
+        # AUTOENCODER
+        tf.reset_default_graph()
+        tf.set_random_seed(42)
+
+        self.X = tf.placeholder(tf.float32, shape=[None, input_size])
+        self.hidden = fully_connected(self.X, encoding_size, activation_fn=tf.nn.elu)
+        outputs = fully_connected(self.hidden, input_size, activation_fn=None)
+
+        reconstruction_loss = tf.reduce_sum(tf.square(outputs - self.X)) # MSE
+
+        optimizer = tf.train.AdamOptimizer(0.0001)
+        self.training_op = optimizer.minimize(reconstruction_loss)
+
+        init = tf.global_variables_initializer()
+
+        self.sess = tf.InteractiveSession()
+        self.sess.run(init)
+
+    def train(self,data,num_iters=100,batch_size=100):
+        for iteration in range(num_iters):
+            for i in range(int(len(data)/batch_size)+1): # BATCH
+                batch = data[i*batch_size: (i+1)*batch_size]
+                self.sess.run([self.training_op], feed_dict={self.X: batch})
+
+    def encode(self,sample):
+        return self.sess.run([self.hidden], feed_dict={self.X: sample})[0]
+
+def loadSCOPTest():
+    # SCOP TEST PDBs
+    structures = []
+    for pdb in os.listdir("SCOP_Test/")[:5]:
+        if not pdb.endswith(".pdb"):
+            continue
+        pdb_path = os.path.join("SCOP_Test", pdb)
+        parser = PDB.PDBParser()
+        structure = parser.get_structure(pdb, pdb_path)
+        structures.append(structure)
+
+    matrixdict = DistanceMatrixDict(structures, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
+    scop_pdb_samples, scop_features = list(matrixdict.keys()), list(matrixdict.values())
+    return scop_pdb_samples, scop_features
+
+# LOAD SCOPTest pdbs
+scop_pdb_samples, scop_features = loadSCOPTest()
 
 train_acc_1,test_acc_1, scop_acc_1 = [],[],[]
 train_acc_5,test_acc_5, scop_acc_5 = [],[],[]
-
 for fold in range(1,11):
     train_pdbs = []
     with open("Folds/{n}_train.txt".format(n=fold),"r") as f:
@@ -62,73 +124,45 @@ for fold in range(1,11):
         for i in f.readlines():
             test_pdbs.append(i.strip("\n"))
 
+    train_pdbs = train_pdbs[:10]
+    test_pdbs = test_pdbs[:10]
     print(len(train_pdbs))
     print(len(test_pdbs))
 
     train_features, test_features = [],[]
     train_pdb_samples, test_pdb_samples = [],[]
 
-
-    for i in range(int(len(train_pdbs)/batch_size)+1): # BATCH
-        structures = []
-        for pdb in train_pdbs[i*batch_size: (i+1)*batch_size]: # EACH PDB IN THE BATCH
-            if pdb == ".ipynb_checkpoints":
-                continue
-            pdb_path = os.path.join("PDBs", pdb)
-            parser = PDB.PDBParser()
-            structure = parser.get_structure(pdb, pdb_path)
-            structures.append(structure)
-        matrixdict = DistanceMatrixDict(structures, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
+    # TRAIN FEATURES
+    for pdb in train_pdbs: # EACH PDB IN THE BATCH
+        if pdb == ".ipynb_checkpoints":
+            continue
+        pdb_path = os.path.join("PDBs", pdb)
+        parser = PDB.PDBParser()
+        structure = parser.get_structure(pdb, pdb_path)
+        matrixdict = DistanceMatrixDict([structure], resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
         pdbs, features = list(matrixdict.keys()), list(matrixdict.values())
-
         train_pdb_samples+=pdbs
         train_features+=features
 
     input_size = len(train_features[0])
 
     # SCALING
-    train_features = np.array(train_features)
-    mean = np.mean(train_features.flatten())
-    std_dev = np.std(train_features.flatten())
+    mean, std_dev = calculate_mean_std(train_features)
     train_features = scale_features(train_features,mean,std_dev)
 
     # AUTOENCODER
-    tf.reset_default_graph()
-    tf.set_random_seed(42)
-
-    X = tf.placeholder(tf.float32, shape=[None, input_size])
-    hidden = fully_connected(X, encoding_size, activation_fn=tf.nn.elu)
-    outputs = fully_connected(hidden, input_size, activation_fn=None)
-
-    reconstruction_loss = tf.reduce_sum(tf.square(outputs - X)) # MSE
-
-    optimizer = tf.train.AdamOptimizer(0.0001)
-    training_op = optimizer.minimize(reconstruction_loss)
-
-    codings = hidden # the output of the hidden layer provides the codings
-
-    init = tf.global_variables_initializer()
-
-    sess = tf.InteractiveSession()
-    sess.run(init)
-
-    # AUTOENCODER TRAINING
-    for iteration in range(num_iters):
-        for i in range(int(len(train_features)/batch_size)+1): # BATCH
-            batch = train_features[i*batch_size: (i+1)*batch_size]
-            sess.run([training_op], feed_dict={X: batch})
+    autoencoder = Autoencoder() # create an autoencoder model
+    autoencoder.build_model() # build model and run session
+    autoencoder.train(train_features,num_iters=100,batch_size=100) # train the model
 
     # TEST FEATURES
-    for i in range(int(len(test_pdbs)/batch_size)+1): # BATCH
-        structures = []
-        for pdb in test_pdbs[i*batch_size: (i+1)*batch_size]: # EACH PDB IN THE BATCH
-            if pdb == ".ipynb_checkpoints":
-                continue
-            pdb_path = os.path.join("PDBs", pdb)
-            parser = PDB.PDBParser()
-            structure = parser.get_structure(pdb, pdb_path)
-            structures.append(structure)
-        matrixdict = DistanceMatrixDict(structures, resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
+    for pdb in test_pdbs: # EACH PDB IN THE BATCH
+        if pdb == ".ipynb_checkpoints":
+            continue
+        pdb_path = os.path.join("PDBs", pdb)
+        parser = PDB.PDBParser()
+        structure = parser.get_structure(pdb, pdb_path)
+        matrixdict = DistanceMatrixDict([structure], resize_strategy=strategy, resize_to=(filter_size,filter_size),removeSymmetry=True)
         pdbs, features = list(matrixdict.keys()), list(matrixdict.values())
         test_pdb_samples+=pdbs
         test_features+=features
@@ -140,72 +174,25 @@ for fold in range(1,11):
     # REPORT
     print("number of train pdbs: {n}".format(n=len(train_pdb_samples)))
     print("number of test pdbs: {n}".format(n=len(test_pdb_samples)))
+
     # Embedding vectors of train and test set
-    new_train_features = sess.run([hidden], feed_dict={X: train_features})[0]
-    new_test_features = sess.run([hidden], feed_dict={X: test_features})[0]
-    new_scop_features = sess.run([hidden], feed_dict={X: scop_features})[0]
+    new_train_features = autoencoder.encode(train_features)
+    new_test_features = autoencoder.encode(test_features)
+    new_scop_features = autoencoder.encode(scop_features)
+    # Create pdb:feature dictionaries
+    train_feature_dict = create_fdict(train_pdb_samples,new_train_features)
+    test_feature_dict = create_fdict(test_pdb_samples,new_test_features)
+    scop_feature_dict = create_fdict(scop_pdb_samples,new_scop_features)
+    # X and y sets are prepared
+    X_train, y_train = X_y(train_feature_dict,label_dict)
+    X_test, y_test = X_y(test_feature_dict,label_dict)
+    X_scop, y_scop = X_y(scop_feature_dict,scop_label_dict)
 
-    train_feature_dict = {}
-    for i in enumerate(train_pdb_samples):
-        if "sample" in i[1]:
-            pdb = i[1].split("sample")[0]
-        else:
-            pdb = i[1]
-        train_feature_dict.setdefault(pdb,[])
-        train_feature_dict[pdb].append(new_train_features[i[0]])
-
-    test_feature_dict = {}
-    for i in enumerate(test_pdb_samples):
-        if "sample" in i[1]:
-            pdb = i[1].split("sample")[0]
-        else:
-            pdb = i[1]
-        test_feature_dict.setdefault(pdb,[])
-        test_feature_dict[pdb].append(new_test_features[i[0]])
-
-    scop_feature_dict = {}
-    for i in enumerate(scop_pdb_samples):
-        if "sample" in i[1]:
-            pdb = i[1].split("sample")[0]
-        else:
-            pdb = i[1]
-        scop_feature_dict.setdefault(pdb,[])
-        scop_feature_dict[pdb].append(new_scop_features[i[0]])
-
-
-    X_train = []
-    y_train = []
-
-    for pdb,vector in train_feature_dict.items():
-        pdb = pdb.split(".")[0]
-        X_train.append(np.average(vector,axis=0))
-        y_train.append(".".join(label_dict[pdb].split(".")[:2]))
-
-    X_test = []
-    y_test = []
-
-    for pdb,vector in test_feature_dict.items():
-        pdb = pdb.split(".")[0]
-        X_test.append(np.average(vector,axis=0))
-        y_test.append(".".join(label_dict[pdb.split(".")[0]].split(".")[:2]))
-
-    X_scop = []
-    y_scop = []
-
-    for pdb,vector in scop_feature_dict.items():
-        pdb = pdb.split(".")[0]
-        X_scop.append(np.average(vector,axis=0))
-        y_scop.append(".".join(scop_label_dict[pdb.split(".")[0]].split(".")[:2]))
-
-
+    # Encoding fold labels
     uniques = list(set(y_train).union(set(y_test)).union(set(y_scop)) )
     group2id = dict(zip(uniques, range(len(uniques))))
-
-    X_train = np.array(X_train)
     y_train = np.array(list(map(lambda x: group2id[x], y_train)))
-    X_test = np.array(X_test)
     y_test = np.array(list(map(lambda x: group2id[x], y_test)))
-    X_scop = np.array(X_scop)
     y_scop = np.array(list(map(lambda x: group2id[x], y_scop)))
 
     # CLASSIFICATION
